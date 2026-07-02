Passw/ziglang-zig

commit d4b70505551ecf568f8af7375cb0e6e5aea97cfb

cmake: remove --zig-lib-dir arg from zig build
This demonstrates one of the downsides of the approach in this branch:
since `zig build` CLI is intentionally unaware of the command line
parsing that will be done by the child process, it cannot obtain zig lib
dir override from the command line even as it needs that information in
order to build the subcommand.

Consequently, zig lib dir has to be passed via env var. CMake does not
provide a way to do this. So instead we rely on Zig's automatic lookup
functionality.

This could be mitigated by adding CLI arg parsing that happens *before*
the subcommand. I find this kind of special case to be awkward and
unpleasant however. I think it's better to simply remove the ability to
pass `--zig-lib-dir` to `zig build`.

CMakeLists.txt
@@ -729,8 +729,6 @@ endif()
 
 
 set(ZIG_BUILD_ARGS
-  --zig-lib-dir "${PROJECT_SOURCE_DIR}/lib"
-
   "-Dversion-string=${RESOLVED_ZIG_VERSION}"
   "-Dtarget=${ZIG_TARGET_TRIPLE}"
   "-Dcpu=${ZIG_TARGET_MCPU}"

ci/aarch64-freebsd-debug.sh
@@ -40,12 +40,14 @@ unset CXX
 
 ninja install
 
+# Must be done after zig cc is finished.
+export ZIG_LIB_DIR="$PWD/../lib"
+
 stage3-debug/bin/zig build test docs \
   --maxrss ${ZSF_MAX_RSS:-0} \
   -Dstatic-llvm \
   -Dskip-non-native \
   --search-prefix "$PREFIX" \
-  --zig-lib-dir "$PWD/../lib" \
   --test-timeout 4m
 
 stage3-debug/bin/zig build \

ci/aarch64-freebsd-release.sh
@@ -40,12 +40,14 @@ unset CXX
 
 ninja install
 
+# Must be done after zig cc is finished.
+export ZIG_LIB_DIR="$PWD/../lib"
+
 stage3-release/bin/zig build test docs \
   --maxrss ${ZSF_MAX_RSS:-0} \
   -Dstatic-llvm \
   -Dskip-non-native \
   --search-prefix "$PREFIX" \
-  --zig-lib-dir "$PWD/../lib" \
   --test-timeout 4m
 
 # Ensure that stage3 and stage4 are byte-for-byte identical.

ci/aarch64-linux-debug.sh
@@ -42,6 +42,9 @@ unset CXX
 
 ninja install
 
+# Must be done after zig cc is finished.
+export ZIG_LIB_DIR="$PWD/../lib"
+
 # No -fqemu and -fwasmtime here as they're covered by the x86_64-linux scripts.
 stage3-debug/bin/zig build test docs \
   --maxrss ${ZSF_MAX_RSS:-0} \
@@ -49,7 +52,6 @@ stage3-debug/bin/zig build test docs \
   -Dskip-non-native \
   -Dtarget=native-native-musl \
   --search-prefix "$PREFIX" \
-  --zig-lib-dir "$PWD/../lib" \
   -Denable-superhtml \
   --test-timeout 3m
 

ci/aarch64-linux-release.sh
@@ -42,6 +42,9 @@ unset CXX
 
 ninja install
 
+# Must be done after zig cc is finished.
+export ZIG_LIB_DIR="$PWD/../lib"
+
 # No -fqemu and -fwasmtime here as they're covered by the x86_64-linux scripts.
 stage3-release/bin/zig build test docs \
   --maxrss ${ZSF_MAX_RSS:-0} \
@@ -49,7 +52,6 @@ stage3-release/bin/zig build test docs \
   -Dskip-non-native \
   -Dtarget=native-native-musl \
   --search-prefix "$PREFIX" \
-  --zig-lib-dir "$PWD/../lib" \
   -Denable-superhtml \
   --test-timeout 3m
 

ci/aarch64-macos-debug.sh
@@ -43,9 +43,11 @@ cmake .. \
 
 ninja install
 
+# Must be done after zig cc is finished.
+export ZIG_LIB_DIR="$PWD/../lib"
+
 stage3-debug/bin/zig build test docs \
   --maxrss ${ZSF_MAX_RSS:-0} \
-  --zig-lib-dir "$PWD/../lib" \
   -Denable-macos-sdk \
   -Dstatic-llvm \
   -Dskip-spirv \

ci/aarch64-macos-release.sh
@@ -43,8 +43,10 @@ cmake .. \
 
 ninja install
 
+# Must be done after zig cc is finished.
+export ZIG_LIB_DIR="$PWD/../lib"
+
 stage3-release/bin/zig build test docs \
-  --zig-lib-dir "$PWD/../lib" \
   -Denable-macos-sdk \
   -Dstatic-llvm \
   -Dskip-spirv \

ci/aarch64-netbsd-debug.sh
@@ -40,12 +40,14 @@ unset CXX
 
 ninja install
 
+# Must be done after zig cc is finished.
+export ZIG_LIB_DIR="$PWD/../lib"
+
 stage3-debug/bin/zig build test docs \
   --maxrss ${ZSF_MAX_RSS:-0} \
   -Dstatic-llvm \
   -Dskip-non-native \
   --search-prefix "$PREFIX" \
-  --zig-lib-dir "$PWD/../lib" \
   --test-timeout 4m
 
 stage3-debug/bin/zig build \

commit 71ee8ceed9dd045aa7a4bb50291fceb05257314b

compiler: jit commands support --zig-lib=[path]
when it is the first parameter after the command name.

fixes regression introduced in d4b70505551ecf568f8af7375cb0e6e5aea97cfb

CMakeLists.txt
@@ -729,6 +729,8 @@ endif()
 
 
 set(ZIG_BUILD_ARGS
+  "--zig-lib=${PROJECT_SOURCE_DIR}/lib"
+
   "-Dversion-string=${RESOLVED_ZIG_VERSION}"
   "-Dtarget=${ZIG_TARGET_TRIPLE}"
   "-Dcpu=${ZIG_TARGET_MCPU}"
@@ -791,7 +793,7 @@ set(ZIG2_WORKING_DIR "${PROJECT_SOURCE_DIR}")
 
 add_custom_command(
   OUTPUT "${PROJECT_BINARY_DIR}/stage3/bin/zig"
-  COMMAND zig2 build --prefix "${PROJECT_BINARY_DIR}/stage3" ${ZIG_BUILD_ARGS}
+  COMMAND zig2 build ${ZIG_BUILD_ARGS} --prefix "${PROJECT_BINARY_DIR}/stage3"
   COMMENT "Building stage3"
   WORKING_DIRECTORY "${ZIG2_WORKING_DIR}"
 )

cmake/install.cmake
@@ -1,4 +1,4 @@
-set(ZIG_INSTALL_ARGS build --prefix "${CMAKE_INSTALL_PREFIX}" ${ZIG_BUILD_ARGS})
+set(ZIG_INSTALL_ARGS build ${ZIG_BUILD_ARGS} --prefix "${CMAKE_INSTALL_PREFIX}")
 execute_process(
   COMMAND "${ZIG_EXECUTABLE}" ${ZIG_INSTALL_ARGS}
   WORKING_DIRECTORY "${ZIG2_WORKING_DIR}"

lib/compiler/Maker.zig
@@ -316,6 +316,8 @@ pub fn main(init: process.Init.Minimal) !void {
                 try forks.append(arena, .init(rest));
             } else if (mem.eql(u8, arg, "--fork")) {
                 try forks.append(arena, .init(nextArgOrFatal(args, &arg_i)));
+            } else if (mem.startsWith(u8, arg, "--zig-lib=")) {
+                fatal("--zig-lib= argument is special and must be first", .{});
             } else if (mem.eql(u8, arg, "-h") or mem.eql(u8, arg, "--help")) {
                 help_menu = true;
             } else if (mem.eql(u8, arg, "-l") or mem.eql(u8, arg, "--list-steps")) {

lib/compiler/Maker/ScannedConfig.zig
@@ -342,6 +342,7 @@ pub fn printUsage(sc: *const ScannedConfig, graph: *Graph, w: *Writer) !void {
         \\  --build-file [file]          Override path to build.zig
         \\  --cache-dir [path]           Override path to local Zig cache directory
         \\  --global-cache-dir [path]    Override path to global Zig cache directory
+        \\  --zig-lib=[arg]              Override path to Zig lib directory
         \\  --seed [integer]             For shuffling dependency traversal order (default: random)
         \\  --cache-poison[=mode]        Override configuration caching behavior
         \\      pure                     (default) Avoid false positive cache hits

src/main.zig
@@ -4932,8 +4932,17 @@ fn jitCmdInner(
     else
         options.release_mode;
     const strip = optimize_mode != .Debug;
-    const override_lib_dir: ?[]const u8 = EnvVar.ZIG_LIB_DIR.get(environ_map);
+    var override_lib_dir: ?[]const u8 = EnvVar.ZIG_LIB_DIR.get(environ_map);
     const override_global_cache_dir: ?[]const u8 = EnvVar.ZIG_GLOBAL_CACHE_DIR.get(environ_map);
+
+    // Special case: if first arg starts with --zig-lib= then it is handled here.
+    var args_i: usize = 0;
+    if (args.len - args_i != 0) {
+        if (mem.cutPrefix(u8, args[args_i], "--zig-lib=")) |rest| {
+            override_lib_dir = rest;
+            args_i += 1;
+        }
+    }
 
     const cwd_path = try std.zig.getResolvedCwd(io, arena);
 
@@ -4952,7 +4961,7 @@ fn jitCmdInner(
     defer dirs.deinit(io);
 
     var child_argv: std.ArrayList([]const u8) = .empty;
-    try child_argv.ensureUnusedCapacity(arena, args.len + 6);
+    try child_argv.ensureUnusedCapacity(arena, (args.len - args_i) + 6);
 
     // We want to release all the locks before executing the child process, so we make a nice
     // big block here to ensure the cleanup gets run when we extract out our argv.
@@ -5064,7 +5073,7 @@ fn jitCmdInner(
     if (options.prepend_seed)
         child_argv.appendAssumeCapacity(try arena.print("--seed=0x{x}", .{randInt(io, u32)}));
 
-    child_argv.appendSliceAssumeCapacity(args);
+    child_argv.appendSliceAssumeCapacity(args[args_i..]);
 
     if (EnvVar.ZIG_VERBOSE_CMD.isSet(environ_map)) {
         const cmd: std.zig.SubprocessCommand = .{